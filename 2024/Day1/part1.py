4d81fec80cc82bd40015fc879a14f3e9e893090c
Aerospaced91 <aerospaceosrs@gmail.com>
2025-07-31 08:27:29 -0700

    Day 1 Part 1

---
+++ 2024/Day1/part1.py
@@ -0,0 +1,19 @@
+left_lst = []
+right_lst = []
+
+with open('data/input.txt', 'r') as f:
+    for line in f:
+        parts = line.strip().split()
+        if len(parts) >= 2: #Ensure at least 2 columns
+            left_lst.append(int(parts[0]))
+            right_lst.append(int(parts[1]))
+
+def part1(left, right):
+    sorted_left = sorted(left)
+    sorted_right = sorted(right)
+
+    differences = [abs(a - b) for a, b in zip(sorted_left, sorted_right)]
+    return sum(differences)
+
+
+print(part1(left_lst, right_lst))
